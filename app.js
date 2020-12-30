a7e4a8725883c6e16bf877cc9e644eb0f384360c
ajityadav15399 <ajityadav.15399@gmail.com>
2020-12-30 21:42:07 +0530

    Completed:{Setup, authentication using passport, User can add and view own story }

---
+++ app.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+
+// Load config
+dotenv.config({path: './config/config.env'})
+
+// PassPort config
+require('./config/passport')(passport)
+connectDB()
+
+const app = express()
+
+//Body parser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+// Logging
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+const {formatDate} = require('./helper/hbs')
+
+// Handlebars
+app.engine(
+    '.hbs',
+     exphbs({ 
+        helpers:{
+             formatDate
+            }, 
+        defaultLayout: 'main', 
+        extname: '.hbs'})
+)
+
+app.set('view engine', '.hbs')
+
+//Sessions
+app.use(session({
+    secret: 'keyword books',
+    resave: false,
+    saveUninitialized:false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// routes
+app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
+app.use('/stories',require('./routes/stories'))
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} on ${PORT}`)
+    );
